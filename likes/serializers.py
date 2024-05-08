d02a0d5c182bdbdbfe2b12287a03a03899729180
Amanda <apita1423@outlook.com>
2024-05-08 19:48:26 +0100

    Add LikeList and LikeDetail to views.py

--- likes/serializers.py
+++ likes/serializers.py
@@ -2,7 +2,7 @@ from django.db import IntegrityError
 from rest_framework import serializers
 from likes.models import Like
 
-class LikesSerializer(serializers.ModelSerializer):
+class LikeSerializer(serializers.ModelSerializer):
     """
     Serializer for the Like model.
     The create method handles the unique constraint on 'owner' and 'post'.
