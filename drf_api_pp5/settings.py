71ff050fd809a5fccb2c4864c37b0893f33b64f3
Amanda <apita1423@outlook.com>
2024-05-13 19:56:46 +0100

    Add heroku link to ALLOWED_HOSTS

--- drf_api_pp5/settings.py
+++ drf_api_pp5/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://cc-drf-api-pp5-b19f7ab60297.herokuapp.com/']
 
 
 # Application definition
