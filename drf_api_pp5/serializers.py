79cb6370ea0c4e0991589cc9482346a622e998af
Amanda <apita1423@outlook.com>
2024-05-16 08:29:23 +0100

    Put new files from settings.py through the pep8 linter

--- drf_api_pp5/serializers.py
+++ drf_api_pp5/serializers.py
@@ -1,6 +1,7 @@
 from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
 
+
 # Code Credit: DRF API Walkthrough
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
